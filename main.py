85a5ae13b6db5351364332f91b1d3db146d9a21b
Jaideep_Mourya <kathalajaideep@gmail.com>
2025-04-21 15:16:14 +0530

    Create main.py
    Solves Part A and Part B

---
+++ main.py
@@ -0,0 +1,8 @@
+from part_a import run_part_a
+from part_b import run_part_b
+
+print("\n--- Part A: Dice Combinations and Probabilities ---")
+run_part_a()
+
+print("\n--- Part B: Undoing Loki's Curse ---")
+run_part_b()
